99640c49b18d13e5ddecec5c68a8688668081d13
Aslan <urusov_1@icloud.com>
2024-04-05 04:30:51 +0400

    javaneto2 changed

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Начисление бонуса:");
+        int amount = 250;
+        int enrolledamount = 1600;
+
+        int bonus;
+        if( enrolledamount >1000) {
+            bonus = enrolledamount / 10;
+        } else {
+            bonus = 0;
+
+        }
+
+        System.out.println(" Бонус за зачисление 1600 рублей составляет:");
+
+        }
+
+    }
+
+
