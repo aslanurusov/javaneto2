98af6e0d08fe980027631c2ad479911a99ad1c5a
Aslan <urusov_1@icloud.com>
2024-04-05 04:36:11 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Начисление бонуса:");
         int amount = 250;
-        int enrolledamount = 1600;
-
+        int creditedamount = 1600;
+        System.out.println(" Бонус за зачисление 1600 рублей составляет:");
         int bonus;
-        if( enrolledamount >1000) {
-            bonus = enrolledamount / 10;
+        if( creditedamount >1000) {
+            System.out.println( bonus = creditedamount / 10 );
         } else {
             bonus = 0;
 
+
         }
 
-        System.out.println(" Бонус за зачисление 1600 рублей составляет:");
 
         }
 
