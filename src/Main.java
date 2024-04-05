7e0b60f255ff1c8f44eb9f384aed3d13e85783d6
Aslan <urusov_1@icloud.com>
2024-04-05 14:09:12 +0400

    src/Main/java changed

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Начисление бонуса:");
         int amount = 250;
-        int creditedamount = 1600;
+        System.out.println("Изначальная сумма на счету : " + amount + "рублей");
+        int creditedAmount = 1600;
+        System.out.println("Зачисление на счет суммы в " + creditedAmount + "рублей");
         System.out.println(" Бонус за зачисление 1600 рублей составляет:");
         int bonus;
-        if (creditedamount > 1000) {
-            System.out.println(bonus = creditedamount / 10);
+        if (creditedAmount > 1000) {
+            System.out.println(bonus = creditedAmount / 10);
         } else {
             bonus = 0;
 
